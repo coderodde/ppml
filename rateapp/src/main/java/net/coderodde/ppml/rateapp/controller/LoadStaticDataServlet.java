876d99bc44f706795837c6f68f51c884947185a3
Rodion Efremov <rodion.efremov@helsinki.fi>
2015-02-11 16:11:12 +0200

    Fighting.

--- rateapp/src/main/java/net/coderodde/ppml/rateapp/controller/LoadStaticDataServlet.java
+++ rateapp/src/main/java/net/coderodde/ppml/rateapp/controller/LoadStaticDataServlet.java
@@ -23,7 +23,7 @@ public class LoadStaticDataServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.getOutputStream().println("Yooo!");
-        response.getOutputStream().println(getStaticDataPath());
+//        response.getOutputStream().println(getStaticDataPath());
     }
 
     /**
