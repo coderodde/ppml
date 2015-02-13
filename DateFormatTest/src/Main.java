bb048160bac42f8d0ab7112e0b0c53353f51c976
Rodion Efremov <rodion.efremov@helsinki.fi>
2015-02-13 14:55:31 +0200

    Extracting dates.

--- DateFormatTest/src/Main.java
+++ DateFormatTest/src/Main.java
@@ -8,11 +8,8 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
         
-        Date d = sdf.parse("01-Jan-1995");
-        
-        System.out.println(d);
+        System.out.println(Integer.parseInt("03"));
     }
     
 }
--- rateapp/src/main/java/net/coderodde/ppml/loader/DataLoader.java
+++ rateapp/src/main/java/net/coderodde/ppml/loader/DataLoader.java
@@ -15,6 +15,9 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataLoader {
 
@@ -23,7 +26,25 @@ public class DataLoader {
     private static final int GENRE_ATTRIBUTES = 2;
     private static final int RATING_ATTRIBUTES = 4;
     private static final String DATE_FORMAT = "dd-MMM-yyyy";
-//    private static final String DATE_FORMAT = "F-M-y";
+    
+    private static final Map<String, Integer> monthMap;
+    
+    static {
+        monthMap = new HashMap<String, Integer>();
+        
+        monthMap.put("Jan", 0);
+        monthMap.put("Feb", 1);
+        monthMap.put("Mar", 2);
+        monthMap.put("Apr", 3);
+        monthMap.put("May", 4);
+        monthMap.put("Jun", 5);
+        monthMap.put("Jul", 6);
+        monthMap.put("Aug", 7);
+        monthMap.put("Sep", 8);
+        monthMap.put("Oct", 9);
+        monthMap.put("Nov", 10);
+        monthMap.put("Dec", 11);
+    }
     
     public static List<User> loadUsers(final String path) {
         final File file = new File(path);
@@ -195,19 +216,11 @@ public class DataLoader {
         Date videoReleaseDate = null;
         
         if (!releaseDateString.isEmpty()) {
-            try {
-                releaseDate = formatter.parse(releaseDateString);
-            } catch (final ParseException pe) {
-                
-            }
+            releaseDate = extractDate(releaseDateString);
         }
         
         if (!videoReleaseDateString.isEmpty()) {
-            try {
-                videoReleaseDate = formatter.parse(videoReleaseDateString);
-            } catch (final ParseException pe) {
-                
-            }
+            videoReleaseDate = extractDate(videoReleaseDateString);
         }
         
         return new Movie(movieId,
@@ -232,6 +245,26 @@ public class DataLoader {
         final long timestamp = Long.parseLong(parts[3].trim());
         
         return new Rating(userId, movieId, score, timestamp);
+    }
+    
+    private static Date extractDate(final String dateString) {
+        final Date ret = new Date();
+        
+        final String[] parts = dateString.split("-");
+        
+        if (parts.length != 3) {
+            return null;
+        }
+        
+        final int day = Integer.parseInt(parts[0]);
+        final int month = monthMap.get(parts[1]);
+        final int year = Integer.parseInt(parts[2]);
+        
+        final Calendar c = Calendar.getInstance();
+       
+        c.set(year, month, day);
+        
+        return new Date(c.getTimeInMillis());
     }
     
     /**
